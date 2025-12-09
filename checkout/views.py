dbbbc24a625303019f611241d8400d81238fb206
madlp24 <120478064+madlp24@users.noreply.github.com>
2025-12-09 15:38:39 -0500

    Remove Stripe secret key from code and use env variables

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from .forms import OrderForm
 
-# Create your views here.
+def checkout(request):
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, "There's nothing in your bag at the moment")
+        return redirect(reverse('products'))
+    
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51ScW95IOVKcQqZO4hrYcwSjkkapOwyEqq51uk3Ohz0oI9BXQERFj6mWqinxB5zp2JUogTlnEHg0H6nb1YUkXrnnx00bRaNwSnQ',
+        'client_secret': 'test cliente secret',
+    }
+
+    return render(request, template, context)
