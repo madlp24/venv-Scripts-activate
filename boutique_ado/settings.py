bf3a70ceedda07577520cd98d07cf613ed024bc5
madlp24 <120478064+madlp24@users.noreply.github.com>
2025-12-12 18:34:12 -0500

    Fixed region name

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -180,7 +180,7 @@ USE_TZ = True
 if 'USE_AWS' in os.environ:
     # Bucket config
     AWS_STORAGE_BUCKET_NAME = 'boutique-ado-pj'
-    AWS_S3_REGION_NAME = 'us-east-1'
+    AWS_S3_REGION_NAME = 'us-east-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
